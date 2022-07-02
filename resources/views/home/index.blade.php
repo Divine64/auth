4290d64fdb47211322996cb7c9ca876a9018963c
divine64 <divineetim416@gmail.com>
2022-07-02 13:14:50 -0700

    Added update on resources

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -14,6 +14,13 @@
         <h1>Dashboard</h1>
         <p class="lead">Only Google Adsence authenticated users can access this section.</p>
         <a class="btn btn-lg btn-primary" href="https://www.google.com/adsense/signup" role="button">View your earnings here &raquo;</a>
+        <h1>Balance:</h1>
+        <p class="lead">$2000</p>
+        <h1>Update:</h1>
+        <p>Withdrawal Form Would be available here on the stated date.</p>
+        <h1>Time:</h1>
+        <p class="lead">10:49pm - 12:30am, GMT, West Africa.</p>
+        <p>Wednesday 6th July, 2022.</p>
         @endauth
 
         @guest
